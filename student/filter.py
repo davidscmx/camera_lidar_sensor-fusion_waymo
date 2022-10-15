Added correct 6-dim covariance matrix

student/filter.py
@@ -39,8 +39,18 @@ class Filter:
 
     @property
     def Q(self):
-        return np.matrix([[0, 0],
-                        [0, 0]])
+        q = self.q
+        dt = self.dt
+        q1 = ((dt**3)/3) * q
+        q2 = ((dt**2)/2) * q
+        q3 = dt * q
+        return np.matrix([[q1, 0, 0, q2, 0, 0],
+                          [0, q1, 0, 0, q2, 0],
+                          [0,  0, q1, 0, 0, q2],
+                          [q2, 0, 0, q3, 0, 0],
+                          [0, q2, 0, 0, q3,0],
+                          [0,  0, q2, 0, 0, q3],
+                          ])
 
     def predict(self, track):
         x = self.F @ track.x