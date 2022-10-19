fab37e94c14b786a113d81660e53e4371c8cf1ed
David Sosa <david.sosa@protonmail.com>
2022-10-19 15:44:11 +0200

    Renaming in association

--- student/association.py
+++ student/association.py
@@ -22,111 +22,114 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 import misc.params as params
+from typing import List
 
 class Association:
     '''Data association class with single nearest neighbor association and gating based on Mahalanobis distance'''
     def __init__(self):
         self.association_matrix = np.matrix([])
         self.unassigned_tracks = []
-        self.unassigned_meas = []
+        self.unassigned_measurements = []
 
-    def associate(self, track_list, meas_list, KF):
+    def associate(self, tracks: List, measurements: List, KF):
+
+        ############
+        # TODO Step 3: association:
+        # - replace association_matrix with the actual association matrix based on Mahalanobis distance (see below) for all tracks and all measurements
+        # - update list of unassigned measurements and unassigned tracks
+        ############
+
         # the following only works for at most one track and one measurement
         self.association_matrix = np.matrix([]) # reset matrix
-        N = len(track_list) # N tracks
-        M = len(meas_list) # M measurements
-
-        # initialize association matrix
-        self.association_matrix = np.inf*np.ones((N,M))
-
-        # loop over all tracks and all measurements to set up association matrix
-        for i in range(N):
-            track = track_list[i]
-            for j in range(M):
-                meas = meas_list[j]
-                dist = self.MHD(track, meas, KF)
-                self.association_matrix[i,j] = dist
-
         self.unassigned_tracks = [] # reset lists
-        self.unassigned_meas = []
+        self.unassigned_measurements = []
 
-        if len(meas_list) > 0:
-            self.unassigned_meas = [0]
-        if len(track_list) > 0:
+        if len(measurements) > 0:
+            self.unassigned_measurements = [0]
+        if len(tracks) > 0:
             self.unassigned_tracks = [0]
-        if len(meas_list) > 0 and len(track_list) > 0:
+        if len(measurements) > 0 and len(tracks) > 0:
             self.association_matrix = np.matrix([[0]])
 
+        ############
+        # END student code
+        ############
+
     def get_closest_track_and_meas(self):
-        # find closest track and measurement for next update
-        A = self.association_matrix
-        if np.min(A) == np.inf:
-            return np.nan, np.nan
+        ############
+        # TODO Step 3: find closest track and measurement:
+        # - find minimum entry in association matrix
+        # - delete row and column
+        # - remove corresponding track and measurement from unassigned_tracks and unassigned_meas
+        # - return this track and measurement
+        ############
 
-        # get indices of minimum entry
-        ij_min = np.unravel_index(np.argmin(A, axis=None), A.shape)
-        ind_track = ij_min[0]
-        ind_meas = ij_min[1]
+        # the following only works for at most one track and one measurement
+        update_track = 0
+        update_measurement = 0
 
-        # delete row and column for next update
-        A = np.delete(A, ind_track, 0)
-        A = np.delete(A, ind_meas, 1)
-        self.association_matrix = A
-
-        # update this track with this measurement
-        update_track = self.unassigned_tracks[ind_track]
-        update_meas = self.unassigned_meas[ind_meas]
-
-        # remove this track and measurement from list
+        # remove from list
         self.unassigned_tracks.remove(update_track)
-        self.unassigned_meas.remove(update_meas)
+        self.unassigned_measurement.remove(update_measurement)
+        self.association_matrix = np.matrix([])
 
-        return update_track, update_meas
+        ############
+        # END student code
+        ############
+        return update_track, update_measurement
 
     def gating(self, MHD, sensor):
-        limit = chi2.ppf(params.gating_threshold, df)
-        if MHD < limit:
-            return True
-        else:
-            return False
+        ############
+        # TODO Step 3: return True if measurement lies inside gate, otherwise False
+        ############
+
+        pass
+
+        ############
+        # END student code
+        ############
 
     def MHD(self, track, meas, KF):
-        H = meas.sensor.get_H(track.x)
-        S = KF.S(track, meas)
-        gamma = KF.gamma(track, meas)
-        MHD = gamma.transpose()*np.linalg.inv(S)*gamma
-        return MHD
+        ############
+        # TODO Step 3: calculate and return Mahalanobis distance
+        ############
 
-    def associate_and_update(self, manager, meas_list, KF):
+        pass
+
+        ############
+        # END student code
+        ############
+
+    def associate_and_update(self, manager, measurements, KF):
         # associate measurements and tracks
-        self.associate(manager.track_list, meas_list, KF)
-
+        self.associate(manager.tracks, measurements, KF)
+        print("associate_and_update ")
         # update associated tracks with measurements
         while self.association_matrix.shape[0]>0 and self.association_matrix.shape[1]>0:
 
             # search for next association between a track and a measurement
-            ind_track, ind_meas = self.get_closest_track_and_meas()
-            if np.isnan(ind_track):
+            track_idx, measurement_idx = self.get_closest_track_and_measurement()
+            if np.isnan(track_idx):
                 print('---no more associations---')
                 break
-            track = manager.track_list[ind_track]
+            track = manager.tracks[track_idx]
 
-            # check visibility, only update tracks in fov
-            if not meas_list[0].sensor.in_fov(track.x):
+            if not measurements[0].sensor.in_fov(track.x):
                 continue
 
-            # Kalman update
-            print('update track', track.id, 'with', meas_list[ind_meas].sensor.name, 'measurement', ind_meas)
-            KF.update(track, meas_list[ind_meas])
-
-            # update score and track state
-            manager.handle_updated_track(track)
+            print('update track', track.id, 'with', measurements[measurement_idx].sensor.name,
+                  'measurement', measurement_idx)
+            KF.update(track, measurements[measurement_idx])
+            manager.update_track(track)
 
             # save updated track
-            manager.track_list[ind_track] = track
+            manager.tracks[track_idx] = track
 
         # run track management
-        manager.manage_tracks(self.unassigned_tracks, self.unassigned_meas, meas_list)
+        manager.set_unassigned_tracks(self.unassigned_tracks)
+        manager.set_unassigned_measurements(self.unassigned_measurements)
+        manager.set_measurments_list(measurements)
+        manager.manage_tracks()
 
-        for track in manager.track_list:
+        for track in manager.tracks:
             print('track', track.id, 'score =', track.score)
